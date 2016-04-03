81a0b2a9b822d15250fefc470c61c8169249fe8e
Josmar Eduardo <josmar@mailkdm.com>
2016-04-02 23:03:35 -0430

    modificaciones
    modificaciones sin importancia (a nivel de comentario y orden de codigo)

--- controladores/iniciodesesion.php
+++ controladores/iniciodesesion.php
@@ -11,7 +11,7 @@ $con="admin";
 //creacion de objeto de la clase Informacionusuario.
 $usuario= new InformacionUsuario();
 
-//se valida inicio de sesion y se guardan los datos de usuario en el array;
+//se valida inicio de sesion y se guardan los datos de usuario en el array en un solo procedimiento
 $datosusuario = $usuario->iniciodesesion($us,$con);
 
 if($datosusuario=='error'){
