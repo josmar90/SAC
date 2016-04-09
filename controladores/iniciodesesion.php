0e1843af99355f8b2cac16232833d4ec422edcab
Josmar Eduardo <josmar@mailkdm.com>
2016-04-09 18:36:40 -0430

    inicio de sesion
    implementacion de controlador de inicio de sesion

--- controladores/iniciodesesion.php
+++ controladores/iniciodesesion.php
@@ -14,11 +14,19 @@ $usuario= new InformacionUsuario();
 //se valida inicio de sesion y se guardan los datos de usuario en el array en un solo procedimiento
 $datosusuario = $usuario->iniciodesesion($us,$con);
 
+//error cuando el usuario no existe en el sistema
 if($datosusuario=='error'){
 echo "error al iniciar sesion";	
 }
 
+//error cuando el usuario existe pero aun no tiene autorizacion para ingresar al sistema
+else if($datosusuario=='error2'){
+echo "error usuario sin acceso valido";	
+}
+
+//usuario existe y posee autorizacion para ingresar al sistema
 else{
 print_r($datosusuario);	
+
 }
 ?>
